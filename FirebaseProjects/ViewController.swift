1e110a77c7e66b61c480114d6aada516a8554fc8
Mostafa <mostafatarek15894@gmail.com>
2019-10-17 01:40:38 -0700

    Initial Commit

---
+++ FirebaseProjects/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirebaseProjects
+//
+//  Created by Mustafa on 10/17/19.
+//  Copyright © 2019 Mostafa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
